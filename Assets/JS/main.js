407b2ba72b0110207a6202e7006dbc1ec8edc6cd
fire8327 <fire83274@gmail.com>
2024-05-31 01:37:00 +0300

    create rates

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -9,4 +9,36 @@ $(".news-container").each((i, el) => {
             $(el).find(".news-toggler").text("Показать меньше");
         }
     })
-})
+});
+
+
+/* получение курса валют */
+let usd, rub = null
+$(document).ready(() => {    
+    const url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Ошибка с запросом!');
+        }
+        return response.json();
+    })
+    .then(data => {
+        usd = data.Valute.USD.Value;
+        $("#currencyFrom").val(1);
+        $("#currencyTo").val(usd.toFixed(3))
+
+        /* смена валют */
+        $("#currencyFrom").change(() => {
+            $("#currencyTo").val(($("#currencyFrom").val() * usd).toFixed(3));
+        });
+        $("#currencyTo").change(() => {
+            $("#currencyFrom").val(($("#currencyTo").val() / usd).toFixed(3));
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+
